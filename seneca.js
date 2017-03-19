f22f98213881de9d17201203f876d1c50fd16826
Len Isac Seneca <len_isac@hotmail.com>
2017-03-19 13:21:07 -0400

    fixed Unexpected unnamed function

--- seneca.js
+++ seneca.js
@@ -14,7 +14,7 @@ program
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
-exports.isValidEmail = function (email) {
+exports.isValidEmail = function isValidEmail(email) {
   const regex = /^\w+@myseneca.ca$/;
   return regex.test(email) ? 'valid' : 'invalid';
 };
@@ -23,7 +23,7 @@ exports.isValidEmail = function (email) {
  * Given a string `name`, return a formatted Seneca email address for
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
-exports.formatSenecaEmail = function (name) {
+exports.formatSenecaEmail = function formatSenecaEmail(name) {
   return `${name}@myseneca.ca`;
 };
 
