be24508135f3a89e02717fbb5710371407907664
Len Isac Seneca <len_isac@hotmail.com>
2017-03-19 12:33:21 -0400

    implemented stub functions isValidEmail & formatSenecaEmail with command line argument processing

--- seneca.js
+++ seneca.js
@@ -1,9 +1,24 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+/**
+ * Process command line arguments - using npm commander library
+ */
+program
+.arguments("")
+ .option('-v, --verify <email>', 'verifies the email address given as a Seneca email')
+ .option('-f, --format <name>', 'formats the name given as a Seneca email')
+ .parse(process.argv);
+
 /**
  * Given a string `email`, return `true` if the string is in the form
  * of a valid Seneca College email address, `false` othewise.
  */
 exports.isValidEmail = function(email) {
-    // TODO: needs to be implemented
+    var regex = /^\w+\@myseneca.ca$/
+    var isValid = regex.test(email) ? 'valid' : 'invalid';
+    return isValid;
 };
 
 /**
@@ -11,5 +26,17 @@ exports.isValidEmail = function(email) {
  * this person. NOTE: the email doesn't need to be real/valid/active.
  */
 exports.formatSenecaEmail = function(name) {
-    // TODO: needs to be implemented
+    return name + "@myseneca.ca";
 };
+
+var email = program.verify;
+var name = program.format;
+
+if (program.verify) {
+    console.log("email: " + email);
+    console.log(exports.isValidEmail(email));
+}
+if (program.format) {
+    console.log("name: " + name);
+    console.log(exports.formatSenecaEmail(name));
+}
