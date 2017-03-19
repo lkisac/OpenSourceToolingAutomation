63865695fc38ac4ea9f6eb1aa09c61c1ca635260
Len Isac Seneca <len_isac@hotmail.com>
2017-03-19 13:12:25 -0400

    fixed 'isValid' is never reassigned. Use 'const' instead

--- seneca.js
+++ seneca.js
@@ -15,9 +15,8 @@ program
  * of a valid Seneca College email address, `false` othewise.
  */
 exports.isValidEmail = function (email) {
-  const regex = /^\w+@myseneca.ca$/;
-  let isValid = regex.test(email) ? 'valid' : 'invalid';
-  return isValid;
+  const regex = /^\w+@myseneca.ca$/;  
+  return regex.test(email) ? 'valid' : 'invalid';
 };
 
 /**
